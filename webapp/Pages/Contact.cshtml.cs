643fb1ca4a98101ab83b6c1773e4541508e6de9a
Eugeniu Ceban <83615676+DMIT-CSD-Eugeniu-Ceban@users.noreply.github.com>
2022-02-18 00:01:46 -0700

    s

--- webapp/Pages/Contact.cshtml.cs
+++ webapp/Pages/Contact.cshtml.cs
@@ -15,8 +15,13 @@ namespace MyApp.Namespace
         public int Number2 { get; set; }
         [BindProperty]
         public int Number3 { get; set; }
-        
-
+        [BindProperty]
+        public string Email { get; set; }
+        [BindProperty]
+        public DateTime MyDate { get; set; }
+        public List<string> SelectListOfSubjects { get; set; }
+        [BindProperty]
+        public int SelectedSubjectId { get; set; }
 
 
 
