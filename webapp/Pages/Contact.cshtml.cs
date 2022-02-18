d100877fba265f940a45311d094d9187a96b0178
Eugeniu Ceban <83615676+DMIT-CSD-Eugeniu-Ceban@users.noreply.github.com>
2022-02-17 22:47:49 -0700

    m

--- webapp/Pages/Contact.cshtml.cs
+++ webapp/Pages/Contact.cshtml.cs
@@ -5,6 +5,17 @@ namespace MyApp.Namespace
 {
     public class ContactModel : PageModel
     {
+        public string Text1{ get; set; }
+        public string Text2 { get; set; }
+        public string Text3 { get; set; }
+        
+
+
+
+        public string ErrorMessage { get; set; }
+        public string SucessMessage { get; set; }
+
+
         public void OnGet()
         {
         }
